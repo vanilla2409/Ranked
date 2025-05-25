cf16196906a8492f6c9cdfd142a73cf20140b0ec
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-05-25 11:35:16 +0530

    Implement cookie parser for authentication

--- server/server.js
+++ server/server.js
@@ -12,11 +12,14 @@ import { checkPlayer } from './helpers/redisPlayersManagement.js'
 import { addToMatchmakingQueue, getMatchForPlayer, matchmakerWorker } from './helpers/redisMatchMaking.js'
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+}))
 app.use(express.json())
 
 
-app.use('/api/users', userRouter)
+app.use('/users', userRouter)
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -160,5 +163,5 @@ app.get('/status-fm' , async (req, res) => {
 
 app.listen(port, async () => {
   console.log(`Server listening on port: ${port}`)
-  await matchmakerWorker();
+  // await matchmakerWorker();
 })
