3e303879dd0e5bde925246537efd7d39f5737a8f
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-05-26 15:53:29 +0530

    Fix middleware. It should now check cookie and decode the user-details
    - middleware will decode and append all the user info to req.user.*

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,14 +2,27 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 const auth = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'No token provided.' });
+  console.log('Auth middleware triggered');
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ success: false, message: 'No token provided' });
   try {
+
     const decoded = jwt.verify(token, JWT_SECRET);
+    const currentTime = Math.floor(Date.now() / 1000);
+    const timeRemaining = decoded.exp - currentTime;
+    if(timeRemaining <= 15 * 60) { // 15 minutes
+      const newToken = jwt.sign({ userId: decoded.userId, username: decoded.username }, JWT_SECRET, { expiresIn: '1d' }); 
+
+      res.cookie('token', newToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'Strict',
+      });
+    }
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Invalid token.' });
+    res.status(401).json({ success: false, message: 'Session Expired. Please login to continue' });
   }
 };
 
